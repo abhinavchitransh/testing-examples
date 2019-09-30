a6c8e747edc79b7673b2ba482170f650c0b854e9
Abhinav Kumar <abhinav.chitransh@gmail.com>
2019-09-30 23:43:00 +0200

    using timeout and performance unit test

--- src/test/java/com/pik/contact/unit/ContactPerformanceTest.java
+++ src/test/java/com/pik/contact/unit/ContactPerformanceTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ContactPerformanceTest {
 
-    @Test (timeout = 1)
+    @Test (timeout = 0)
     public void can_be_updated() {
         Contact contact = aContact("John", "Doe");
 
